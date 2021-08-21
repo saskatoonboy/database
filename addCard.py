8038ffefa436f8283431ce78819b4bfc4dbed6f6
saskatoonboy <54918014+saskatoonboy@users.noreply.github.com>
2021-08-21 11:13:38 -0600

    new cards

--- addCard.py
+++ addCard.py
@@ -1,14 +1,19 @@
+lastName = None
+while True:
+    name = input("What is the id code?\n")
+    if name == "":
+        name = lastName
+    else:
+        lastName = name
 
-name = input("What is the id code?\n")
+    cardFile = open(name+"/index.html", "r")
+    data = cardFile.read()
+    cardFile.close()
 
-cardFile = open(name+"/index.html", "r")
-data = cardFile.read()
-cardFile.close()
+    index = data.index("Own")
+    count = int(data[index+4:index+5])+1
+    data = data[:index+4]+str(count)+data[index+5:]
 
-index = data.index("Own")
-count = int(data[index+4:index+5])+1
-data = data[:index+4]+str(count)+data[index+5:]
-
-cardFile = open(name+"/index.html", "w")
-cardFile.write(data)
-cardFile.close()
+    cardFile = open(name+"/index.html", "w")
+    cardFile.write(data)
+    cardFile.close()
